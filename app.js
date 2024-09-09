3435b62dec3a403f28e38641e5a30055751c40e6
Rachit Jain <rachitjain9596@gmail.com>
2024-09-09 14:42:07 +0530

    app

--- app.js
+++ app.js
@@ -2,3 +2,30 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store api</h1> <a href="/api/v1/products">products</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async (req, res) => {
+  try {
+    // connect DB
+    app.listen(port, console.log(`Server listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
