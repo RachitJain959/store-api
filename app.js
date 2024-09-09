e274d758bec8229118dd5fd8b6ebe1e557dd4d80
Rachit Jain <rachitjain9596@gmail.com>
2024-09-09 18:05:55 +0530

    async-errors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -15,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store api</h1> <a href="/api/v1/products">products</a>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
