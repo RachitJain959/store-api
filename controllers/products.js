37ff85732342525398c3adf21e557c87d598156e
Rachit Jain <rachitjain9596@gmail.com>
2024-09-11 15:31:21 +0530

    models-productSchema

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,4 @@
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async errors");
   res.status(200).json({ msg: "Product testing route" });
 };
 
---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "product name required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price required"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      value: ["ikea", "liddy", "marcos"],
+      message: "{VALUE} not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
